345d06a87d0c4b7f883a72e51bef8b9fcfd3ebe8
Christopher Esterhuyse <christopher.esterhuyse@gmail.com>
2023-07-18 09:31:10 +0200

    borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use crate::parse::rule;
 use std::collections::HashMap;
 
 mod parse;
 
-#[derive(Debug, Eq, PartialEq, Hash)]
+#[derive( Debug, Eq, PartialEq, Hash)]
 pub enum Atom {
     Tuple(Vec<Atom>),
     Id(String),
@@ -16,23 +15,91 @@ pub struct Rule {
     antecedents: Vec<Atom>,
 }
 
+#[derive(Debug)]
 struct ProofTree<'a> {
-    consequent: Atom,
     rule: &'a Rule,
-    antecedents: Vec<ProofTree<'a>>,
-    substitutions: HashMap<&'a String, Atom>,
+    sub: Substitutions<'a>,
+    lemmata: Vec<ProofTree<'a>>,
 }
 
-impl ProofTree<'_> {
+#[derive(Default,Debug)]
+struct Substitutions<'a> {
+    map: HashMap<u16,&'a Atom>,
+}
+impl<'a> Substitutions<'a> {
+    fn sub(&mut self, k: u16, v: &'a Atom) -> bool {
+        match self.map.get(&k).copied() {
+            Some(v2) if v2 != v => false,
+            _ => {
+                self.map.insert(k, v);
+                true
+            }
+        }
+    }
+    fn reset(&mut self) {
+        self.map.clear()
+    }
+}
+
+impl Atom {
+    fn sub_vars<'a:'c,'b,'c>(&'a self, to_match: &'a Self, sub: &'b mut Substitutions<'c>) -> bool {
+        match [self, to_match] {
+            [Self::Id(a), Self::Id(b)] => a == b, 
+            [Self::Tuple(a), Self::Tuple(b)] if a.len() == b.len() => {
+                for (a,b) in a.iter().zip(b.iter()) {
+                    if !a.sub_vars(b,sub) {
+                        return false
+                    }
+                }
+                true
+            }
+            [Self::Variable(a), Self::Variable(b)] if a == b => {
+                true
+            }
+            [Self::Variable(a), b] => {
+                sub.sub(*a, b)
+            }
+            _ => false
+        }
+    }
+}
+
+impl<'a> ProofTree<'a> {
     fn check(&self) -> bool {
         todo!()
     }
-    fn create(rules: &[Rule], atom: &Atom) -> Option<Self> {
-        todo!()
+    fn create(rules: &'a [Rule], goal: &'a Atom) -> Option<Self> {
+        let mut sub = Substitutions::default();
+        for rule in rules {
+            if rule.consequent.sub_vars(goal, &mut sub) {
+                let maybe_lemmata = rule.antecedents.iter().map(|subgoal| {
+                    ProofTree::create(rules, subgoal)
+                }).collect();
+                if let Some(lemmata) = maybe_lemmata {
+
+                    return Some(Self {
+                        rule, lemmata, sub
+                    })
+                }
+            }
+            sub.reset();
+        }
+        None
     }
 }
 
 fn main() {
-    let rule = rule(" A  :- (x) (x y (z ((q(h))))).");
-    println!("{:?}", rule);
+    let rules = parse::wsr(parse::rules)(
+    "
+    (b 2).
+    (a 0) :- (b 0).
+    ",
+    ).unwrap().1;
+    println!("{:#?}", rules);
+    let goal = parse::wsr(parse::atom)("
+    (a lel)
+    ").unwrap().1;
+    println!("{:#?}", goal);
+    let proof = ProofTree::create(&rules, &goal);
+    println!("{:#?}", proof);
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,7 +1,7 @@
 use crate::Atom;
 use crate::Rule;
 use nom::branch::alt;
-use nom::character::complete::alphanumeric1;
+use nom::character::complete::{alphanumeric1,u16 as nomu16};
 use nom::character::complete::multispace0;
 use nom::character::complete::satisfy;
 use nom::combinator::map as nommap;
@@ -9,13 +9,11 @@ use nom::combinator::opt;
 use nom::combinator::recognize;
 use nom::error::ParseError;
 use nom::multi::many0;
+use nom::multi::many0_count;
 use nom::sequence::delimited;
+use nom::sequence::pair;
 use nom::sequence::preceded;
 use nom::sequence::terminated;
-
-use nom::multi::many0_count;
-use nom::multi::separated_list0;
-use nom::sequence::pair;
 use nom::{bytes::complete::tag, IResult};
 
 pub fn wsl<'a, F, O, E>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
@@ -26,6 +24,17 @@ where
     preceded(multispace0, inner)
 }
 
+
+pub fn wsr<'a, F, O, E>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+where
+    E: ParseError<&'a str>,
+    F: FnMut(&'a str) -> IResult<&'a str, O, E> + 'a,
+{
+    terminated(inner, multispace0)
+}
+
+
+
 pub fn atom(s: &str) -> IResult<&str, Atom> {
     let t = |f: fn(&char) -> bool| {
         recognize(pair(
@@ -33,37 +42,23 @@ pub fn atom(s: &str) -> IResult<&str, Atom> {
             many0_count(alt((tag("_"), tag("-"), alphanumeric1))),
         ))
     };
-    let id = nommap(t(char::is_ascii_lowercase), |c: &str| {
-        Atom::Id(c.to_owned())
-    });
+    let id = nommap(t(char::is_ascii_lowercase), |c: &str| Atom::Id(c.to_owned()));
     let tuple = delimited(
         tag("("),
-        nommap(many0(atom), |mut x| {
-            if x.len() == 1 {
-                x.pop().unwrap()
-            } else {
-                Atom::Tuple(x)
-            }
-        }),
+        nommap(many0(atom), |mut x| if x.len() == 1 { x.pop().unwrap() } else { Atom::Tuple(x) }),
         wsl(tag(")")),
     );
-    let var = nommap(t(char::is_ascii_uppercase), |c: &str| {
-        Atom::Id(c.to_owned())
-    });
+    let var = nommap(nomu16, Atom::Variable);
     wsl(alt((id, tuple, var)))(s)
 }
 
+pub fn rules(s: &str) -> IResult<&str, Vec<Rule>> {
+    many0(rule)(s)
+}
+
 pub fn rule(s: &str) -> IResult<&str, Rule> {
-    let (s, (consequent, maybe_antecedents)) = terminated(
-        pair(atom, opt(preceded(wsl(tag(":-")), many0(atom)))),
-        wsl(tag(".")),
-    )(s)?;
+    let (s, (consequent, maybe_antecedents)) =
+        terminated(pair(atom, opt(preceded(wsl(tag(":-")), many0(atom)))), wsl(tag(".")))(s)?;
     let antecedents = maybe_antecedents.unwrap_or_default();
-    Ok((
-        s,
-        Rule {
-            consequent,
-            antecedents,
-        },
-    ))
+    Ok((s, Rule { consequent, antecedents }))
 }
